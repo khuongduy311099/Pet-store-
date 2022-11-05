3b926ddb3ffb93346c8b01e9c9407df7fe305eff
KhuongDuy <kd31z1099@gmail.com>
2022-11-05 12:35:09 +0700

    ?

--- src/App.js
+++ src/App.js
@@ -1,29 +1,30 @@
-import Layout, { Content } from "antd/lib/layout/layout";
-import { Route, Routes } from "react-router-dom";
+import Layout from "antd/lib/layout/layout";
+import {Route, Routes} from "react-router-dom";
 import TotalCartItem from "./Components/cartIcon/totalCartItem";
 import LayoutFooter from "./Layout/LayoutComponents/Footer/footer";
 import LayoutHeader from "./Layout/LayoutComponents/Header/header";
 import Product from "./Layout/LayoutComponents/Product/product";
 import LoginLayout from "./Layout/LoginLayout/Login";
-import MainLayout from "./Routes/Home";
+import MainLayout from "./Layout/MainLayout";
 import RegisterLayout from "./Layout/RegisterLayout/RegisterLayout";
-
+import Cagetory from "./Routes/Cagetory";
+import CategoryDetailItem from "./Routes/Cagetory/CategoryDetail";
+import CategoryDetailItems from "./Routes/Cagetory/CategoryDetail";
+import {dogData} from "./Data";
+import ProductItem from "./Layout/LayoutComponents/Product/product";
 function App() {
-  return (
-    <Layout>
-      <LayoutHeader />
-      <Content style={{ padding: "150px 50px", backgroundColor: "white" }}>
-        <Routes>
-          <Route path="/" element={<MainLayout />} />
-          <Route path="/login" element={<LoginLayout />} />
-          <Route path="/register" element={<RegisterLayout />} />
-          <Route path="/product" element={<Product />} />
-        </Routes>
-      </Content>
-
-      <LayoutFooter />
-    </Layout>
-  );
+    return (
+        <div>
+            <LayoutHeader />
+            <Routes>
+                <Route path="/" element={<MainLayout />} />
+                <Route path="/login" element={<LoginLayout />} />
+                <Route path="/register" element={<RegisterLayout />} />
+                <Route path="/product" element={<Product />} />
+            </Routes>
+            <LayoutFooter />
+        </div>
+    );
 }
 
 export default App;
